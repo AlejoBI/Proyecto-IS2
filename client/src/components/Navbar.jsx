e83b7ccd0da13c52e8857d3a29a624f49ab34ff8
Laab1611 <ludy.agudelo@uao.edu.co>
2024-11-11 07:43:42 -0500

    Adelanto frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,77 @@
-import "../assests/css/Navbar.css";
+import { Container, Nav, Navbar, Button, Image, NavDropdown } from "react-bootstrap";
+import "../assests/css/Navbar.css"; 
+import { useAuth } from "../context/AuthContext";
+import Logo from "../assests/images/Logo.jpg";
 
-const Navbar = () => {
+const NavbarComponent = () => {
+  const { isAuthenticated, logout, user } = useAuth();
+  const title = user ? `${user.username}` : "Home";
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <span className="text-primary">RAG</span> System
-        </a>
-      </div>
-    </nav>
+    <Navbar
+      style={{
+        backgroundColor: "white",
+        padding: "0.3% 8%",
+      }}
+      variant="dark"
+      expand="lg"
+    >
+      <Container>
+        <Navbar.Brand href="/">
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Image
+              src={Logo}
+              alt="Logo"
+              height="70"
+              className="d-inline-block align-top"
+            />
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-dark-example" />
+        <Navbar.Collapse id="navbar-dark-example">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              width: "100%",
+              paddingLeft: "28%",
+            }}
+          >
+          </div>
+          <Nav style={{ justifyContent: "flex-end" }}>
+            {isAuthenticated ? (
+              <NavDropdown title={title} id="navbarScrollingDropdown">
+                <NavDropdown.Item href="/user/profile">
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  href="/"
+                  onClick={() => {
+                    logout();
+                  }}
+                >
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <>
+              <Button
+                href="/login"
+                style={{
+                  backgroundColor: "#187A6B",
+                  borderColor: "#187A6B",
+                  marginRight: "10px",
+                  fontWeight: "bold"                }}
+              >
+                Login
+              </Button>
+            </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarComponent;
--- client/src/pages/LoginRegisterPage.jsx
+++ client/src/pages/LoginRegisterPage.jsx
@@ -3,6 +3,7 @@ import FormLogin from "../components/FormLogin";
 import FormRegister from "../components/FormRegister";
 import { Card, Container, Button, Image, Alert } from 'react-bootstrap';
 import { useState } from "react";
+import Logo from "../assests/images/Logo.jpg";
 
 const LoginRegisterPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -13,11 +14,11 @@ const LoginRegisterPage = () => {
   };
 
   return (
-    <Container className="d-flex align-items-center justify-content-center min-vh-100 min-vw-100" style={{ background: "linear-gradient(135deg, #1e3c72, #2a5298)" }}>
+    <Container className="d-flex align-items-center justify-content-center min-vh-100 min-vw-100" style={{ background: "#187A6B" }}>
       <Card className="shadow-lg rounded-5 w-50 h-50 d-flex justify-content-center align-items-center">
         <Card.Body className="w-50 h-50">
           <div className="text-center mb-4">
-            <Image src="path-to-logo" alt="Logo" className="img-fluid" style={{ width: "80px", height: "80px" }} />
+            <Image src={Logo} alt="Logo" className="img-fluid" style={{ width: "80px", height: "80px" }} />
             <h2 className="card-title">{isLogin ? "Login" : "Register"}</h2>
             {errors && <Alert variant="danger">{errors.detail}</Alert>} {/* Muestra errores */}
           </div>
